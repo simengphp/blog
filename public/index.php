5772a81af53466dab7e7105a8a4933dfa16720f8
476319748@qq.com <476319748@qq.com>
2017-11-23 20:32:52 +0800

    test

--- public/index.php
+++ public/index.php
@@ -14,6 +14,6 @@
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
 // 定义应用目录
-define('CONF_PATH', __DIR__ . '/../config/');
+define('CONF_PATH', __DIR__ . '/../app/config/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
